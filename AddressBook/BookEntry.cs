7a880ada6bf84409deb00777287a9ab8f53de96f
QuentinSims <86727861+QuentinSims@users.noreply.github.com>
2021-12-07 09:38:29 +0200

    Add files via upload

---
+++ AddressBook/BookEntry.cs
@@ -0,0 +1,18 @@
+﻿namespace AddressBook
+{
+    class BookEntry
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
+
+        public BookEntry(string firstName, string lasName, string phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lasName;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookEntry bookEntry1 = new BookEntry("Quentin", "Sims", "076 955 3787");
+            Console.WriteLine(bookEntry1.FirstName);
+        }
+    }
+}
